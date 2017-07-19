bab20f48a5a92ab4c82f55cd08e7afc1fa497198
Jianchu <jianchuli0624@gmail.com>
2017-07-19 19:50:44 -0400

    add more docs for BackEnd

--- src/checkers/inference/solver/backend/BackEnd.java
+++ src/checkers/inference/solver/backend/BackEnd.java
@@ -18,11 +18,21 @@ import checkers.inference.model.VariableSlot;
 import checkers.inference.solver.frontend.Lattice;
 
 /**
- * BackEnd class is the super class for all concrete back ends.
+ * BackEnd class is the super class for all concrete back ends. Type constraint
+ * solving process happens inside this class. A back end takes type constraints
+ * from {@link checkers.inference.solver.GeneralSolver}}, solves the
+ * constraints, and return a map between an integer and a annotation mirror as
+ * the inferred result. Method {@link #solve()} is responsible for coordinating
+ * above steps.
+ * 
+ * {@link #solve()} method is the entry point of the back end, and it is got
+ * called in class {@link checkers.inference.solver.GeneralSolver}}. See
+ * {@link checkers.inference.solver.GeneralSolver#solveInparall()} and
+ * {@link checkers.inference.solver.GeneralSolver#solveInSequential()}.
  * 
  * @author jianchu
  *
- * @param <S> Encoding type for slot. 
+ * @param <S> Encoding type for slot.
  * @param <T> Encdoing type for constraint.
  */
 public abstract class BackEnd<S, T> {
@@ -49,8 +59,24 @@ public abstract class BackEnd<S, T> {
         this.lattice = lattice;
     }
 
+    /**
+     * A concrete back end needs to override this method and implements its own
+     * constraint-solving strategy. In general, there will be three steps in this method:
+     * 1. Calls {@link #convertAll()} to convert constraints into
+     * the corresponding encoding form.
+     * 2. Calls an external solver to solve the encoding.
+     * 3. Decodes the solution from the external solver and create a map between an 
+     * Integer(Slot Id) and an AnnotationMirror as it's inferred annotation. 
+     * 
+     * It is the concrete back end's responsibility to implemented the logic of above instructions and statistic collection. 
+     * See {@link checkers.inference.solver.backend.maxsatbackend.MaxSatBackEnd#solve()}} for an example.
+     */
     public abstract Map<Integer, AnnotationMirror> solve();
 
+    /**
+     * Calls serializer to convert constraints into the corresponding encoding
+     * form. See {@link checkers.inference.solver.backend.maxsatbackend.MaxSatBackEnd#convertAll()}} for an example.
+     */
     protected abstract void convertAll();
 
     /**

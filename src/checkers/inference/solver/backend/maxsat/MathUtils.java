b75687e50bd85ad78c24e6b724e67f05f7f01b4c
CharlesZ-Chen <charleszhuochen@gmail.com>
2017-10-31 12:01:27 -0400

    Add javadoc for MathUtil#mapIdToMatrixEntry().

--- src/checkers/inference/solver/backend/maxsat/MathUtils.java
+++ src/checkers/inference/solver/backend/maxsat/MathUtils.java
@@ -10,6 +10,16 @@ import checkers.inference.solver.frontend.Lattice;
  */
 public class MathUtils {
 
+    /**
+     * Given a varSlot id, the integer representation of a type, and the lattice, map these to
+     * an int represents the boolean variable in SAT solver, that represents the case of
+     * this varSlot is the given type. (so the negative of the return value would be
+     * this varSlot is not the given type.)
+     * @param varId the slot id of the varSlot
+     * @param typeInt the integer representation of a type
+     * @param lattice  target lattice
+     * @return an integer represents a boolean variable in SAT solver
+     */
     public static int mapIdToMatrixEntry(int varId, int typeInt, Lattice lattice) {
         int column = typeInt + 1;
         int row = varId - 1;

ced22194d9c39da494811f8e976880425195553d
CharlesZ-Chen <charleszhuochen@gmail.com>
2017-11-09 14:25:02 -0500

    Add downstream test.

--- .travis-build.sh
+++ .travis-build.sh
@@ -6,6 +6,40 @@ set -e
 
 export SHELLOPTS
 
+# Optional argument $1 is one of:
+#   cfi-tests, downstream
+# If it is omitted, this script does everything.
+export GROUP=$1
+if [[ "${GROUP}" == "" ]]; then
+  export GROUP=all
+fi
+
+SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
+if [[ "$SLUGOWNER" == "" ]]; then
+  SLUGOWNER=opprop
+fi
+
+if [[ "${GROUP}" != "cfi-tests" && "${GROUP}" != "downstream" ]]; then
+  echo "Bad argument '${GROUP}'; should be omitted or one of: cfi-tests, downstream."
+  exit 1
+fi
+
 . ./.travis-build-without-test.sh
 
-ant -f tests.xml run-tests
+# Test CF Inference
+if [[ "${GROUP}" == "cfi-tests" || "${GROUP}" == "all" ]]; then
+    ant -f tests.xml run-tests
+fi
+
+# Downstream tests
+if [[ "${GROUP}" == "downstream" || "${GROUP}" == "all" ]]; then
+
+    # Ontology test: 10 minutes
+    echo "Running: (cd $ROOT && git clone --depth 1 https://github.com/opprop/ontology.git)"
+    (cd $ROOT && git clone --depth 1 https://github.com/opprop/ontology.git)
+    echo "... done: (cd $ROOT && git clone --depth 1 https://github.com/opprop/ontology.git)"
+
+    echo "Running: (cd $ROOT/ontology && gradle build -x test && ./test-ontology.sh)"
+    (cd $ROOT/ontology && gradle build -x test && ./test-ontology.sh)
+    echo "... done: (cd $ROOT/ontology && gradle build -x test && ./test-ontology.sh)"
+fi
